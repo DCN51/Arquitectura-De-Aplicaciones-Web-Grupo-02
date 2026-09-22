c789875146c3617085ef274bf69e7e154719642b
nosequepones7 <mauriciopezoj@gmail.com>
2026-09-21 22:03:21 -0500

    feat: initial spring boot project structure

---
+++ src/main/java/pe/edu/upc/arquiwebgrupo02/ArquiWebGrupo02Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.arquiwebgrupo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiWebGrupo02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArquiWebGrupo02Application.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/arquiwebgrupo02/ArquiWebGrupo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.arquiwebgrupo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiWebGrupo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
