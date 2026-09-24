b05efbb2e86b95e48587a729954442c156fac9de
DCN51 <u202322205@upc.edu.pe>
2026-09-24 12:56:03 -0500

    Se agrego los repositorios de Catalogo, Suscripcion y Usuario

---
+++ src/main/java/pe/edu/upc/arquiwebgrupo02/repositories/IUsuarioRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.arquiwebgrupo02.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.upc.arquiwebgrupo02.entities.Usuario;
+
+public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
+}
